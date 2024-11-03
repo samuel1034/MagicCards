96d1fcf5884a2b674c1e21d7aee88f0c46437ed8
Samuel Fuentes (Genius) <samuelgenius1034@gmail.com>
2024-11-03 13:04:50 -0400

    shuffle cards

--- src/App.js
+++ src/App.js
@@ -1,6 +1,29 @@
+import { useState } from 'react'
 import './App.css'
 
+const cardImages = [
+  {"src": "/img/helmet-1.png"},
+  {"src": "/img/potion-1.png"},
+  {"src": "/img/ring-1.png"},
+  {"src": "/img/sword-1.png"},
+  {"src": "/img/scroll-1.png"},
+  {"src": "/img/shield-1.png"}  
+]
+
 function App() {
+
+  const [cards, SetCards] = useState([])
+  const [turns, SetTurns] = useState(0)
+
+  const shuffleCards =  () => {
+    const shuffledCards = [...cardImages, ...cardImages]
+
+      .sort(()  => Math.random() - 0.5)
+      .map((card) => ({...card, id: Math.random()}))
+      setCards(shuffledCards)
+      setTurns(0)
+    }
+
   return (
     <div className="App">
       <h1>Magic Match</h1>
