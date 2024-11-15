8faf345ee076748d633ed46ff8360a1f03b8fd42
Samuel Fuentes (Genius) <samuelgenius1034@gmail.com>
2024-11-15 09:29:17 -0400

    Flipping Cards

--- src/App.js
+++ src/App.js
@@ -50,7 +50,7 @@ function App() {
         });
         resetTurn()
       } else {
-        resetTurn();
+         setTimeout(() =>  resetTurn(), 1000);
       }      
     }
   }, [choiceOne, choiceTwo]);
@@ -77,7 +77,11 @@ function App() {
       <button onClick={shuffleCards}>New Game</button>
       <div className='card-grid'>
         {cards.map(card => (
-          <SingleCard key={card.id} card={card} handleChoice={handleChoice}/>
+          <SingleCard
+           key={card.id} 
+           card={card} 
+           handleChoice={handleChoice}
+           flipped={card === choiceOne || card === choiceTwo || card.matched}/>
         ))}
       </div>
     </div>
